cbcf7eb1d6b3603f4b55825612a3faf845236ba5
Benjamin MAERTEN <benjamin.maerten@isen.yncrea.fr>
2020-05-06 15:48:16 +0200

    LE HELLO WORLD TEST

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.mariuszgromada.math.mxparser.*;
 
 public class Main extends Application {
 
@@ -14,10 +15,17 @@ public class Main extends Application {
         primaryStage.setTitle("Va jouer à leTEST");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+    	System.out.println("application start");
+        Expression eh = new Expression("5^2 * 7^3 * 11^1 * 67^1 * 49201^1");
+        Expression ew = new Expression("71^1 * 218549^1 * 2195547^1");
+        String h = mXparser.numberToAsciiString(eh.calculate() );
+        String w = mXparser.numberToAsciiString(ew.calculate());
+        mXparser.consolePrintln(h + " " + w);
+    	launch(args);
     }
 }
