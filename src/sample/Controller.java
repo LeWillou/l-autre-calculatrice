bd735e72a67b71e04c25165129151f34737eae94
Willou <louis.willems@isen.yncrea.fr>
2020-05-06 16:32:34 +0200

    TextArea + TableView
    Adds expression in tableview, clearing textArea, making mXparser understand we have an expression

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.scene.control.TableView;
 import org.mariuszgromada.math.mxparser.Argument;
 import org.mariuszgromada.math.mxparser.Expression;
 import javafx.event.ActionEvent;
@@ -9,20 +10,25 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import java.awt.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    protected List<String> textAreaElements = new ArrayList<String>();
-    Iterator<String> myIter = textAreaElements.iterator();
-
-
     @FXML public TextArea equationDisplayer;
+    @FXML public TableView expressionsTable;
+    @FXML public Button drawExpression;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Argument x = new Argument("x");
+        drawExpression.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Expression f = new Expression(equationDisplayer.getText(), x);
+                expressionsTable.getItems().add(f);
+                equationDisplayer.clear();
+            }
+        });
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,20 +12,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Va jouer à leTEST");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setTitle("L'autre calculatrice");
+        primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
 
     }
 
 
     public static void main(String[] args) {
-    	System.out.println("application start");
-        Expression eh = new Expression("5^2 * 7^3 * 11^1 * 67^1 * 49201^1");
-        Expression ew = new Expression("71^1 * 218549^1 * 2195547^1");
-        String h = mXparser.numberToAsciiString(eh.calculate() );
-        String w = mXparser.numberToAsciiString(ew.calculate());
-        mXparser.consolePrintln(h + " " + w);
-    	launch(args);
+        launch(args);
     }
 }
