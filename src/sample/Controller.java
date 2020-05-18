d0f2ff44d98c8b6346984bfbb73dac04f331a16d
Willou <louis.willems@isen.yncrea.fr>
2020-05-18 02:48:21 +0200

    ArrayList + graph implementation
    The function is calculated on a -200 / 200 range, adding the results in an array and displaying a graph using these results

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,54 @@
 package sample;
 
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.TableView;
 import org.mariuszgromada.math.mxparser.Argument;
 import org.mariuszgromada.math.mxparser.Expression;
+import org.mariuszgromada.math.mxparser.Function;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import org.mariuszgromada.math.mxparser.mXparser;
 
 import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML public TextArea equationDisplayer;
     @FXML public TableView expressionsTable;
     @FXML public Button drawExpression;
+    @FXML public LineChart graphDisplayer;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Argument x = new Argument("x");
+        mXparser.setDegreesMode();
         drawExpression.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent actionEvent) {
-                Expression f = new Expression(equationDisplayer.getText(), x);
-                expressionsTable.getItems().add(f);
+                ArrayList resultsList = new ArrayList();
+                Function f = new Function("f", equationDisplayer.getText(), "x");
+                Expression a = new Expression(equationDisplayer.getText(), x);
+                expressionsTable.getItems().add(a);
                 equationDisplayer.clear();
+                final XYChart.Series<Double, Double> drawFunction = new XYChart.Series<>();
+                int j = 0;
+                double d;
+                for(int i = -200; i <= 200; i++){
+                    Expression e = new Expression("f(" + Integer.toString(i) + ")", f);
+                    resultsList.add(e.calculate());
+                    d = i;
+                    drawFunction.getData().add(new XYChart.Data<Double, Double>(d, Double.parseDouble(resultsList.get(j).toString())));
+                    System.out.println(resultsList.get(j));
+                    j++;
+                }
+                graphDisplayer.getData().add(drawFunction);
             }
         });
     }
