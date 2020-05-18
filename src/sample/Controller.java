ae249040eb287583c38cdb4b3621a2b2397a182c
Willou <louis.willems@isen.yncrea.fr>
2020-05-18 17:04:30 +0200

    Benjo DL Push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
 import org.mariuszgromada.math.mxparser.Argument;
 import org.mariuszgromada.math.mxparser.Expression;
 import org.mariuszgromada.math.mxparser.Function;
@@ -29,10 +30,31 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Argument x = new Argument("x");
         mXparser.setDegreesMode();
+        ArrayList resultsList = new ArrayList();
         drawExpression.setOnAction(new EventHandler<ActionEvent>(){
             @Override
             public void handle(ActionEvent actionEvent) {
-                ArrayList resultsList = new ArrayList();
+                Function f = new Function("f", equationDisplayer.getText(), "x");
+                Expression a = new Expression(equationDisplayer.getText(), x);
+                expressionsTable.getItems().add(a);
+                equationDisplayer.clear();
+                final XYChart.Series<Double, Double> drawFunction = new XYChart.Series<>();
+                int j = 0;
+                double d;
+                for(int i = -200; i <= 200; i++){
+                    Expression e = new Expression("f(" + Integer.toString(i) + ")", f);
+                    resultsList.add(e.calculate());
+                    d = i;
+                    drawFunction.getData().add(new XYChart.Data<Double, Double>(d, Double.parseDouble(resultsList.get(j).toString())));
+                    System.out.println(resultsList.get(j));
+                    j++;
+                }
+                graphDisplayer.getData().add(drawFunction);
+            }
+        });
+        equationDisplayer.setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.ENTER){
+                keyEvent.consume();
                 Function f = new Function("f", equationDisplayer.getText(), "x");
                 Expression a = new Expression(equationDisplayer.getText(), x);
                 expressionsTable.getItems().add(a);
